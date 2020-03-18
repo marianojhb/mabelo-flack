ed1f4139c0411b6e8bbf7168545224c766d33508
marianojhb <mbelgrano@gmail.com>
2020-03-18 17:18:50 -0300

    all set

---
+++ test_area/colorama_example.py
@@ -0,0 +1,9 @@
+import os
+import colorama
+from colorama import Fore, Style
+
+os.system("clear")
+print(Fore.GREEN + f"The text is in green")
+print(Fore.CYAN + f"The text is in cyan")
+print(Fore.RED) # From now on, text printed will be in red
+print(Fore.CYAN) # From now on, text printed will be in cyan
